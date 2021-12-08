646abc1787dfbf9e2ee61289024c4bd7a6d21ed8
Chloé Mosselmans Ismaily <60384700+cloclo1996@users.noreply.github.com>
2021-12-08 17:27:19 +0100

    Revert "Update SerialPorts.cs"
    This reverts commit 5b97544e294f08675dd852fff32c9f2039bbcefe.

--- Exerscent/Assets/Scripts/Communication/Arduino/SerialPorts.cs
+++ Exerscent/Assets/Scripts/Communication/Arduino/SerialPorts.cs
@@ -10,7 +10,7 @@ public class SerialPorts : MonoBehaviour {
 	public string[] ports;
 	string portName;
 	string wantedPortName="usbmodem";
-	string wantedPortNameWindows="COM";
+	string wantedPortNameWindows="COM3";
 	string fullPortName;
 	public bool portFound=false;
 	public UIManager UIManager;
